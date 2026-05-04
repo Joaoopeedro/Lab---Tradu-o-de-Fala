7ea9e81fdc55a83958b0134a2b20c60d5df75786
Jotape <jotape@MacBook-Pro-de-Jotape.local>
2026-05-04 19:08:27 -0300

    start commit

---
+++ LabReconhecimentoFala/lab.py
@@ -0,0 +1,79 @@
+from datetime import datetime
+import os
+
+# Importar namespaces
+import azure.cognitiveservices.speech as speech_sdk
+
+
+def main():
+    try:
+        global speech_config
+
+        # Obter configurações
+        ai_key = ""
+        ai_region = ""
+
+        # Configurar serviço de fala
+        speech_config = speech_sdk.SpeechConfig(ai_key, ai_region)
+        print('Pronto para utilizar serviço de fala em:', speech_config.region)
+
+
+        # Obter entrada por fala
+        command = TranscribeCommand()
+        if command.lower() == 'what time is it?':
+            TellTime()
+
+    except Exception as ex:
+        print(ex)
+
+def TranscribeCommand():
+    command = ''
+
+    # Configurar reconhecimento de fala
+    audio_config = speech_sdk.AudioConfig(use_default_microphone=True)
+
+    # OPCIONAL: Definir idioma para Português do Brasil
+    speech_config.speech_recognition_language = "pt-BR"
+
+    speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
+    print('Fale agora...')
+
+
+    # Processar entrada de fala
+    speech = speech_recognizer.recognize_once_async().get()
+    if speech.reason == speech_sdk.ResultReason.RecognizedSpeech:
+        command = speech.text
+        print(command)
+    else:
+        print(speech.reason)
+        if speech.reason == speech_sdk.ResultReason.Canceled:
+            cancellation = speech.cancellation_details
+            print(cancellation.reason)
+            print(cancellation.error_details)
+
+
+    # Retornar o comando
+    return command
+
+
+def TellTime():
+    now = datetime.now()
+    response_text = 'The time is {}:{:02d}'.format(now.hour,now.minute)
+
+    # Configurar síntese de fala
+    speech_config.speech_synthesis_voice_name = "pt-BR-DonatoNeural"
+    speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config)
+
+
+    # Sintetizar saída falada
+    speak = speech_synthesizer.speak_text_async(response_text).get()
+    if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
+        print(speak.reason)
+
+
+    # Exibir a resposta
+    print(response_text)
+
+
+if __name__ == "__main__":
+    main()
---
+++ LabReconhecimentoFala/labTraducao.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+import os
+
+# Importar namespaces
+import azure.cognitiveservices.speech as speech_sdk
+
+
+def main():
+    try:
+        global speech_config
+        global translation_config
+
+        # Obter configurações
+        ai_key = ""
+        ai_region = ""
+        
+        # Configurar tradução
+        translation_config = speech_sdk.translation.SpeechTranslationConfig(ai_key, ai_region)
+        translation_config.speech_recognition_language = 'en-US'
+        translation_config.add_target_language('fr')
+        translation_config.add_target_language('es')
+        translation_config.add_target_language('hi')
+        translation_config.add_target_language('pt')
+
+        print('Preparado para traduzir de',translation_config.speech_recognition_language)
+
+
+        # Configurar fala
+        speech_config = speech_sdk.SpeechConfig(ai_key, ai_region)
+
+
+        # Obter entrada do usuário
+        targetLanguage = ''
+        while targetLanguage != 'quit':
+            targetLanguage = input('\nInforme uma linguagem para traduzir\n fr = Francês\n es = Espanhol\n hi = Hindi\n Digite outra opção para parar\n').lower()
+            if targetLanguage in translation_config.target_languages:
+                Translate(targetLanguage)
+            else:
+                targetLanguage = 'quit'
+
+
+    except Exception as ex:
+        print(ex)
+
+def Translate(targetLanguage):
+    translation = ''
+
+    # Traduzir fala
+    audio_config = speech_sdk.AudioConfig(use_default_microphone=True)
+    translator = speech_sdk.translation.TranslationRecognizer(translation_config, audio_config = audio_config)
+    print("Fale agora...")
+    result = translator.recognize_once_async().get()
+    print('Traduzindo"{}"'.format(result.text))
+    translation = result.translations[targetLanguage]
+    print(translation)
+
+
+    # Sintetizar tradução
+    voices = {
+        "fr": "fr-FR-HenriNeural",
+        "es": "es-ES-ElviraNeural",
+        "hi": "hi-IN-MadhurNeural"
+    }
+    translation_config.speech_synthesis_voice_name = voices.get(targetLanguage)
+    speech_synthesizer = speech_sdk.SpeechSynthesizer(translation_config)
+    speak = speech_synthesizer.speak_text_async(translation).get()
+    if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
+        print(speak.reason)
+
+
+
+if __name__ == "__main__":
+    main()
